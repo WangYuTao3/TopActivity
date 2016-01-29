458e7e76ae60545f34b6d712ba548417377408f6
wangyt <wyt3123@163.com>
2016-01-29 16:39:31 +0800

    commit project

---
+++ app/src/main/java/com/example/administrator/topactivity/HookService.java
@@ -0,0 +1,67 @@
+package com.example.administrator.topactivity;
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * Created by wangyt on 2016/1/18.
+ * : description
+ */
+public class HookService extends AccessibilityService {
+
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        //begin hook
+        AccessibilityServiceInfo config = new AccessibilityServiceInfo();
+        config.eventTypes = AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED;
+        config.feedbackType = AccessibilityServiceInfo.FEEDBACK_GENERIC;
+        if (Build.VERSION.SDK_INT >= 16)
+            config.flags = AccessibilityServiceInfo.FLAG_INCLUDE_NOT_IMPORTANT_VIEWS;
+
+        setServiceInfo(config);
+        Log.e("wyt", "onServiceConnected");
+    }
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        Log.e("wyt", "onAccessibilityEvent");
+        //event callback
+        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+            ComponentName componentName = new ComponentName(
+                    event.getPackageName().toString(),
+                    event.getClassName().toString()
+            );
+
+            ActivityInfo activityInfo = tryGetActivity(componentName);
+            boolean isActivity = activityInfo != null;
+            if (isActivity) {
+                Log.e("wyt", componentName.flattenToShortString());
+            }
+        }
+    }
+
+    private ActivityInfo tryGetActivity(ComponentName componentName) {
+        try {
+            return getPackageManager().getActivityInfo(componentName, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public void onInterrupt() {
+        Log.e("wyt", "onInterrupt");
+    }
+}
---
+++ app/src/main/java/com/example/administrator/topactivity/Utils.java
@@ -0,0 +1,159 @@
+package com.example.administrator.topactivity;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Environment;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.List;
+
+/**
+ * Created by wangyt on 2016/1/29.
+ * : description
+ */
+public class Utils {
+
+    public static boolean isAccessibilityEnabled(Context context, String name) {
+        int accessibilityEnabled = 0;
+        boolean accessibilityFound = false;
+        try {
+            accessibilityEnabled = Settings.Secure.getInt(context.getContentResolver(), android.provider.Settings.Secure.ACCESSIBILITY_ENABLED);
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        TextUtils.SimpleStringSplitter mStringColonSplitter = new TextUtils.SimpleStringSplitter(':');
+
+        if (accessibilityEnabled == 1) {
+            String settingValue = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
+            if (settingValue != null) {
+                TextUtils.SimpleStringSplitter splitter = mStringColonSplitter;
+                splitter.setString(settingValue);
+                while (splitter.hasNext()) {
+                    String accessabilityService = splitter.next();
+                    if (accessabilityService.equalsIgnoreCase(name)) {
+                        accessibilityFound = true;
+                    }
+                }
+            }
+        }
+        return accessibilityFound;
+    }
+
+
+    public static String getTopActPkgNameFromProcess(Context context) {
+        ActivityManager.RunningAppProcessInfo currentInfo = null;
+        Field field = null;
+        int START_TASK_TO_FRONT = 2;
+        String pkgName = null;
+        try {
+            field = ActivityManager.RunningAppProcessInfo.class.getDeclaredField("processState");
+        } catch (Exception e) {
+            return null;
+        }
+        ActivityManager am = (ActivityManager) context.getSystemService(Activity.ACTIVITY_SERVICE);
+        if (am == null) {
+            return null;
+        }
+        List<ActivityManager.RunningAppProcessInfo> appList = am.getRunningAppProcesses();
+        if (appList == null || appList.isEmpty()) {
+            return null;
+        }
+        for (ActivityManager.RunningAppProcessInfo app : appList) {
+            if (app != null && app.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                Integer state = null;
+                try {
+                    state = field.getInt(app);
+                } catch (Exception e) {
+                    return null;
+                }
+                if (state != null && state == START_TASK_TO_FRONT) {
+                    currentInfo = app;
+                    break;
+                }
+            }
+        }
+        if (currentInfo != null) {
+            pkgName = currentInfo.processName;
+        }
+        return pkgName;
+    }
+
+    //useless
+    public static String getTopActClassNameFromADB(Context context) {
+        String result = "";
+        try {
+            result = run(new String[]{"dumpsys activity a|grep mFocusedActivity"}, Environment.getExternalStorageDirectory().getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] shorts = result.split(" ");
+        if (shorts.length >= 4) {
+            result = shorts[3];
+        }
+        return result;
+    }
+
+    private static synchronized String run(String[] cmd, String workdirectory)
+            throws IOException {
+        StringBuffer result = new StringBuffer();
+        try {
+            ProcessBuilder builder = new ProcessBuilder(cmd);
+            InputStream in = null;
+            if (workdirectory != null) {
+                builder.directory(new File(workdirectory));
+                builder.redirectErrorStream(true);
+                Process process = builder.start();
+                in = process.getInputStream();
+                byte[] re = new byte[1024];
+                while (in.read(re) != -1) {
+                    result = result.append(new String(re));
+                }
+            }
+            if (in != null) {
+                in.close();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return result.toString();
+    }
+
+    //useless
+    public static String getTopActClassNameFromPkgName(Context context, String packageName) {
+        PackageManager packageManager = context.getPackageManager();
+        Log.e("wyt", "packageName:" + packageName);
+        String result = "empty";
+        try {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.setPackage(packageName);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            Class<?>[] cc = new Class[2];
+            cc[0] = Intent.class;
+            cc[1] = int.class;
+            Method method = PackageManager.class.getDeclaredMethod("resolveActivity", cc);
+            ResolveInfo resolveInfo = (ResolveInfo) method.invoke(packageManager, intent, PackageManager.GET_INTENT_FILTERS);
+            if (resolveInfo != null) {
+                result = resolveInfo.activityInfo.name;
+                Log.e("wyt", "className:" + result);
+            } else {
+                Log.e("wyt", "componentName null");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("wyt", e.getMessage());
+        }
+        return result;
+    }
+}
